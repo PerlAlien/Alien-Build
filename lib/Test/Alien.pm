4fcba4874621ba028c3c2005dabf9d0b8589b03b
Graham Ollis <perl@wdlabs.com>
2015-12-25 07:37:54 -0700

    Test::Alien: start of xs_ok

--- lib/Test/Alien.pm
+++ lib/Test/Alien.pm
@@ -5,10 +5,13 @@ use warnings;
 use 5.008001;
 use Env qw( @PATH );
 use File::Which 1.10 qw( which );
-use Capture::Tiny qw( capture );
+use Capture::Tiny qw( capture capture_merged );
+use File::Temp qw( tempdir );
+use File::Spec;
+use Text::ParseWords qw( shellwords );
 use Test::Stream::Context qw( context );
 use Test::Stream::Exporter;
-default_exports qw( alien_ok run_ok );
+default_exports qw( alien_ok run_ok xs_ok );
 no Test::Stream::Exporter;
 
 # ABSTRACT: Testing tools for Alien modules
@@ -131,6 +134,160 @@ sub run_ok
   $ctx->release;
   
   $run;
+}
+
+=head2 xs_ok
+
+ xs_ok $xs;
+ xs_ok $xs, $message;
+
+Compiles, links the given C<XS> code and attaches to Perl.
+
+C<$xs> may be either a string containing the C<XS> code,
+or a hash reference with these keys:
+
+=over 4
+
+=item xs
+
+The XS code.  This is the only required element.
+
+=item pxs
+
+The L<ExtUtils::ParseXS> arguments passes as a hash reference.
+
+=item verbose
+
+Spew copious debug information via test note.
+
+=cut
+
+sub xs_ok
+{
+  my($xs, $message) = @_;
+  $message ||= 'xs';
+  
+  $xs = { xs => $xs } unless ref $xs;
+  $xs->{pxs} ||= {};
+  my $verbose = $xs->{verbose};
+  my $ok = 1;
+  my @diag;
+  my $dir = tempdir( CLEANUP => 1 );
+  my $xs_filename = File::Spec->catfile($dir, 'test.xs');
+  my $c_filename  = File::Spec->catfile($dir, 'test.c');
+  
+  my $ctx = context();
+  my $module;
+
+  # this regex copied shamefully from ExtUtils::ParseXS
+  # in part because we need the module name to do the bootstrap
+  # and also because if this regex doesn't match then ParseXS
+  # does an exit() which we don't want.
+  if($xs->{xs} =~ /^MODULE\s*=\s*([\w:])+(?:\s+PACKAGE\s*=\s*([\w:]+))?(?:\s+PREFIX\s*=\s*(\S+))?\s*$/m)
+  {
+    $module = $1;
+  }
+  else
+  {
+    $ok = 0;
+    push @diag, '  XS does not have a module decleration that we could find';
+  }
+
+  if($ok)
+  {
+    open my $fh, '>', $xs_filename;
+    print $fh $xs->{xs};
+    close $fh;
+  
+    require ExtUtils::ParseXS;
+    my $pxs = ExtUtils::ParseXS->new;
+  
+    my($out, $err) = capture_merged {
+      eval {
+        $pxs->process_file(
+          filename     => $xs_filename,
+          output       => $c_filename,
+          versioncheck => 0,
+          prototypes   => 0,
+          %{ $xs->{pxs} },
+        );
+      };
+      $@;
+    };
+    
+    $ctx->note("parse xs $xs_filename => $c_filename") if $verbose;
+    $ctx->note($out) if $verbose;
+    $ctx->note("error: $err") if $verbose && $err;
+  
+    unless($pxs->report_error_count == 0)
+    {
+      $ok = 0;
+      push @diag, '  ExtUtils::ParseXS failed:';
+      push @diag, "    $err" if $err;
+      push @diag, "    $_" for split /\r?\n/, $out;
+    }
+  }
+
+  if($ok)
+  {
+    require ExtUtils::CBuilder;
+    my $cb = ExtUtils::CBuilder->new;
+
+    my($out, $obj, $err) = capture_merged {
+      my $obj = eval {
+        $cb->compile(
+          source               => $c_filename,
+          extra_compiler_flags => [shellwords map { $_->cflags } @aliens],
+        );
+      };
+      ($obj, $@);
+    };
+    
+    $ctx->note("compile $c_filename") if $verbose;
+    $ctx->note($out) if $verbose;
+    $ctx->note($err) if $verbose && $err;
+    
+    unless($obj)
+    {
+      $ok = 0;
+      push @diag, '  ExtUtils::CBuilder->compile failed';
+      push @diag, "    $err" if $err;
+      push @diag, "    $_" for split /\r?\n/, $out;
+    }
+    
+    if($ok)
+    {
+    
+      my($out, $lib) = capture_merged {
+        $cb->link(
+          objects            => [$obj],
+          extra_linker_flags => [shellwords map { $_->libs } @aliens],
+        );
+      };
+      
+      $ctx->note("link $obj") if $verbose;
+      $ctx->note($out) if $verbose;
+      
+      if($lib)
+      {
+        $ctx->note("created lib $lib");
+      }
+      else
+      {
+        $ok = 0;
+        push @diag, '  ExtUtils::CBuilder->link failed';
+        push @diag, "    $_" for split /\r?\n/, $out;
+      }
+    
+    }
+
+  }
+
+  $ctx->ok($ok, $message);
+  $ctx->diag($_) for @diag;
+  $ctx->release;
+  
+  $ok;
 }
 
 1;
---
+++ t/test_alien_xs_ok.t
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+use Test::Stream qw( -V1 -Tester Subtest );
+use Test::Alien;
+
+plan 3;
+
+is(
+  intercept { xs_ok '' },
+  array {
+    event Ok => sub {
+      call pass => F();
+      call name => 'xs';
+    };
+    event Diag => sub {
+      call message => '  XS does not have a module decleration that we could find';
+    };
+    end;
+  },
+  'xs with no module'
+);
+
+is(
+  intercept { xs_ok "this should cause a compile error\nMODULE = Foo::Bar PACKAGE = Foo::Bar\n" },
+  array {
+    event Ok => sub {
+      call pass => F();
+      call name => 'xs';
+    };
+    event Diag => sub {
+      call message => '  ExtUtils::CBuilder->compile failed';
+    };
+  },
+  'xs with C compile error'
+);
+
+my $xs = do { local $/; <DATA> };
+xs_ok { xs => $xs, verbose => 1 };
+
+__DATA__
+
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+MODULE = Foo::Bar PACKAGE = Foo::Bar
