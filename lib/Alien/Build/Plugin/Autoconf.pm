b3487f6ca61fbeb2b4fa08a54a1a0197e5eadc5c
Graham Ollis <plicease@cpan.org>
2017-01-19 14:35:53 -0500

    use MSYS make for autoconf on windows

--- lib/Alien/Build/Plugin/Autoconf.pm
+++ lib/Alien/Build/Plugin/Autoconf.pm
@@ -29,15 +29,34 @@ Some reasonable default flags will be provided.
   # TODO:
   #  - --with-pic       on by default
   #  - --disable-shared on by default
-  #  - modify %{make} to be just 'make' on MSWin32 (since you will be using the Alien::MSYS verson);
   #  - AB::P::Autoconf::Shared to build shared library too
+
+  my @msys_reqs = $^O eq 'MSWin32' ? ('Alien::MSYS' => '0.07') : ();
 
   $intr->add_helper(
     configure => sub {
       _win ? 'sh configure' : './configure'
     },
-    ($^O eq 'MSWin32' ? ('Alien::MSYS' => '0.07') : ()),
+    @msys_reqs,
   );
+
+=head2 make
+
+ %{make}
+
+On windows the default C<%{make}> helper is replace with the make that comes with
+L<Alien::MSYS>.  This is almost certainly what you want, as most autoconf projects
+will not build with C<nmake> or C<dmake> typically used by Perl on Windows.
+
+=cut
+  
+  # if we are building something with autoconf, the gmake that comes with
+  # Alien::MSYS is almost certainly preferable to the nmake or dmake that
+  # was used to build Perl
+  $intr->replace_helper(
+    make => sub { 'make' },
+    @msys_reqs,
+  ) if $^O eq 'MSWin32';
   
   $self;
 }
