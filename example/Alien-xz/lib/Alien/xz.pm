fd3d9bf34c9f71feaf3d1a4bb7d92d903c685189
Graham Ollis <plicease@cpan.org>
2017-02-08 13:08:41 -0500

    remove fleshed out Alien-xz example.
    As the real Alien::xz now uses alienfile anyway.

--- example/Alien-xz/lib/Alien/xz.pm
+++
@@ -1,9 +0,0 @@
-package Alien::xz;
-
-use strict;
-use warnings;
-use base qw( Alien::Base2 );
-
-our $VERSION = '0.01';
-
-1;
--- example/Alien-xz/t/basic.t
+++
@@ -1,8 +0,0 @@
-use strict;
-use warnings;
-use Test2::Bundle::Extended;
-use Alien::xz;
-
-ok 1;
-
-done_testing;
