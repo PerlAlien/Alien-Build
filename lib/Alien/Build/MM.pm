e5eff38c6fef6e9671fdd9a406f9195a6ff46820
Graham Ollis <plicease@cpan.org>
2017-02-08 07:54:36 -0500

    bump version for ab::mm

--- lib/Alien/Build/MM.pm
+++ lib/Alien/Build/MM.pm
@@ -109,7 +109,7 @@ sub mm_args
     Carp::croak "DISTNAME is required";
   }
   
-  my $ab_version = '0.10';
+  my $ab_version = '0.11';
   
   $args{CONFIGURE_REQUIRES} = Alien::Build::_merge(
     'Alien::Build::MM' => $ab_version,
