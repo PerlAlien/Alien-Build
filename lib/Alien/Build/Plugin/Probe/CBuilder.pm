b4da255e3654811bd5967c59716153240915af82
Graham Ollis <plicease@cpan.org>
2017-07-06 12:02:39 -0400

    add support for recursive aliens on probe::cbuilder

--- lib/Alien/Build/Plugin/Probe/CBuilder.pm
+++ lib/Alien/Build/Plugin/Probe/CBuilder.pm
@@ -18,6 +18,13 @@ use Capture::Tiny qw( capture_merged capture );
    libs   => '-L/opt/libfoo/lib -lfoo',
  );
 
+alternately:
+
+ ues alienfile;
+ plugin 'Probe::CBuilder' => (
+   aliens => [ 'Alien::libfoo', 'Alien::libbar' ],
+ );
+
 =head1 DESCRIPTION
 
 This plugin probes for compiler and linker flags using L<ExtUtils::CBuilder>.  This is a useful
@@ -69,11 +76,55 @@ test program.
 
 has version => undef;
 
+=head2 aliens
+
+List of aliens to query fro compiler and linker flags.
+
+=cut
+
+has aliens => [];
+
+=head2 lang
+
+The programming language to use.  One of either C<C> or C<C++>.
+
+=cut
+
+has lang => 'C';
+
 sub init
 {
   my($self, $meta) = @_;
   
-  $meta->add_requires('configure' => 'ExtUtils::CBuilder' => 0 );
+  $meta->add_requires('configure' => 'ExtUtils::CBuilder' => 0 );  
+
+  if(@{ $self->aliens })
+  {  
+    die "You can't specify both 'aliens' and either 'cflags' or 'libs' for the Probe::CBuilder plugin" if $self->cflags || $self->libs;
+
+    $meta->add_requires('configure' => $_ => 0 ) for @{ $self->aliens };
+    $meta->add_requires('Alien::Build::Plugin::Probe::CBuilder' => '0.53');
+
+    my $cflags = '';
+    my $libs   = '';
+    foreach my $alien (@{ $self->aliens })
+    {
+      require Module::Load;
+      Module::Load::load($alien);
+      $cflags .= $alien->cflags . ' ';
+      $libs   .= $alien->libs   . ' ';
+    }
+    $self->cflags($cflags);
+    $self->libs($libs);
+  }
+  
+  my @cpp;
+  
+  if($self->lang ne 'C')
+  {
+    $meta->add_requires('Alien::Build::Plugin::Probe::CBuilder' => '0.53');
+    @cpp = ('C++' => 1) if $self->lang eq 'C++';
+  }
   
   $meta->register_hook(
     probe => sub {
@@ -88,19 +139,34 @@ sub init
       
       my $b = ExtUtils::CBuilder->new(%{ $self->options });
 
-      my($out1, $obj) = capture_merged {
+      my($out1, $obj) = capture_merged { eval {
         $b->compile(
           source               => 'mytest.c',
           extra_compiler_flags => $self->cflags,
+          @cpp,
         );
-      };
+      } };
       
-      my($out2, $exe) = capture_merged {
+      if(my $error = $@)
+      {
+        $build->log("compile failed: $error");
+        $build->log("compile failed: $out1");
+        die $@;
+      }
+      
+      my($out2, $exe) = capture_merged { eval {
         $b->link_executable(
           objects              => [$obj],
           extra_linker_flags   => $self->libs,
         );
-      };
+      } };
+      
+      if(my $error = $@)
+      {
+        $build->log("link failed: $error");
+        $build->log("link failed: $out2");
+        die $@;
+      }      
       
       my($out, $err, $ret) = capture { system($^O eq 'MSWin32' ? $exe : "./$exe") };
       die "execute failed" if $ret;
