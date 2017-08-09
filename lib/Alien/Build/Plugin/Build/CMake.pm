377310b14ff183fc7a779b4b349780a99c7a3784
Graham Ollis <plicease@cpan.org>
2017-08-09 15:58:08 -0400

    Alien-Build-Plugin-Build-CMake: add cmake_generator helper

--- lib/Alien/Build/Plugin/Build/CMake.pm
+++ lib/Alien/Build/Plugin/Build/CMake.pm
@@ -3,6 +3,7 @@ package Alien::Build::Plugin::Build::CMake;
 use strict;
 use warnings;
 use 5.008001;
+use Config;
 use Alien::Build::Plugin;
 
 # ABSTRACT: CMake plugin for Alien::Build
@@ -15,7 +16,8 @@ use Alien::Build::Plugin;
  share {
    plugin 'Build::CMake';
    build [
-     '%{cmake} -DCMAKE_INSTALL_PREFIX:PATH=%{.install.prefix} .',
+     # this is the default build step, if you do not specify one.
+     [ '%{cmake}', -G => '%{cmake_generator}',  '-DCMAKE_INSTALL_PREFIX:PATH=%{.install.prefix}', '.' ],
      '%{make}',
      '%{make} install',
    ];
@@ -26,6 +28,16 @@ use Alien::Build::Plugin;
 This plugin helps build alienized projects that use C<cmake>.
 The intention is to make this a core L<Alien::Build> plugin if/when
 it becomes stable enough.
+
+=head1 HELPERS
+
+=head2 cmake
+
+This plugin replaces the default cmake helper with the one that comes from L<Alien::cmake3>.
+
+=head2 cmake_generator
+
+This is the appropriate C<cmake> generator to use based on the make used by your Perl.
 
 =head1 SEE ALSO
 
@@ -48,11 +60,23 @@ sub init
   $meta->prop->{destdir} = 1;
   
   $meta->add_requires('share' => 'Alien::cmake3' => '0.02');
+
   $meta->interpolator->replace_helper('cmake' => sub { require Alien::cmake3; Alien::cmake3->exe });
+  $meta->interpolator->add_helper('cmake_generator' => sub {
+    if($^O eq 'MSWin32')
+    {
+      # once again, Windows gets to be 'special'
+      die 'fixme';
+    }
+    else
+    {
+      return 'Unix Makefiles';
+    }
+  });
 
   $meta->default_hook(
     build => [
-      ['%{cmake}', '-DCMAKE_INSTALL_PREFIX:PATH=%{.install.prefix}', '.' ],
+      ['%{cmake}', -G => '%{cmake_generator}', '-DCMAKE_INSTALL_PREFIX:PATH=%{.install.prefix}', '.' ],
       ['%{make}' ],
       ['%{make}', 'install' ],
     ],
