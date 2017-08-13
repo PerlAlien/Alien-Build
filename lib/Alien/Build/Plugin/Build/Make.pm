9755a0dceed0abd1371c2381946fa0108efaeb3a
Graham Ollis <plicease@cpan.org>
2017-08-13 06:41:55 -0400

    Alien-Build-Plugin-Build-Make: Initial commit

---
+++ lib/Alien/Build/Plugin/Build/Make.pm
@@ -0,0 +1,10 @@
+package Alien::Build::Plugin::Build::Make;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Make plugin for Alien::Build
+# VERSION
+
+1;
---
+++ t/alien_build_plugin_build_make.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use Alien::Build::Plugin::Build::Make;
+
+ok 1, 'todo';
+
+done_testing
