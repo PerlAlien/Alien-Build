433465c8f6f0908127178767be914dbb71fe3b82
Graham Ollis <plicease@cpan.org>
2017-09-25 18:27:22 -0400

    Alien-Build-Plugin-Fetch-CurlCommand: doco

--- lib/Alien/Build/Plugin/Fetch/CurlCommand.pm
+++ lib/Alien/Build/Plugin/Fetch/CurlCommand.pm
@@ -13,6 +13,38 @@ use File::chdir;
 # ABSTRACT: Curl command line plugin for fetching files
 # VERSION
 
+=head1 SYNOPSIS
+
+ use alienfile;
+ 
+ share {
+   meta->prop->{start_url} = 'https://www.openssl.org/source/';
+   plugin 'Fetch::CurlCommand';
+ 
+ };
+
+=head1 DESCRIPTION
+
+This plugin provides a fetch based on the C<curl> command.  It works with other fetch
+plugins (ie. the first one which succeeds will be used).  Most of the time the best plugin
+to use will be L<Alien::Build::Plugin::Download::Negotiate>, but for some SSL bootstraping
+it may be desirable to try C<curl> first.
+
+This plugin is not currently part of the L<Alien::Build> core, but the hope is that it
+will be declared stable enough in the near future to be included.
+
+=head1 PROPERTIES
+
+=head2 curl_command
+
+The full path to the C<curl> command.  The default is usually correct.
+
+=head2 ssl
+
+Ignored by this plugin.  Provided for compatbility with some other fetch plugins.
+
+=cut
+
 has curl_command => sub { which('curl') };
 has ssl => 0;
 has _see_headers => 0;
@@ -95,3 +127,15 @@ sub init
 }
 
 1;
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<alienfile>
+
+=item L<Alien::Build>
+
+=back
+
+=cut
