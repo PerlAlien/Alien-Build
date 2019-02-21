3785897b13ba9351a44c5b99988bce2e849a1f04
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2019-02-21 18:32:43 +0000

    Document ->version_cmp (and move it after the compare methods, so it reads a little neater)

--- lib/Alien/Base.pm
+++ lib/Alien/Base.pm
@@ -362,36 +362,6 @@ sub version {
     };
 }
 
-# Sort::Versions isn't quite the same algorithm because it differs in
-# behaviour with leading zeroes.
-#   See also  https://dev.gentoo.org/~mgorny/pkg-config-spec.html#version-comparison
-sub version_cmp {
-  shift;
-  my @x = (shift =~ m/([0-9]+|[a-z]+)/ig);
-  my @y = (shift =~ m/([0-9]+|[a-z]+)/ig);
-
-  while(@x and @y) {
-    my $x = shift @x; my $x_isnum = $x =~ m/[0-9]/;
-    my $y = shift @y; my $y_isnum = $y =~ m/[0-9]/;
-
-    if($x_isnum and $y_isnum) {
-      # Numerical comparison
-      return $x <=> $y if $x != $y;
-    }
-    elsif(!$x_isnum and !$y_isnum) {
-      # Alphabetic comparison
-      return $x cmp $y if $x ne $y;
-    }
-    else {
-      # Of differing types, the numeric one is newer
-      return $x_isnum - $y_isnum;
-    }
-  }
-
-  # Equal so far; the longer is newer
-  return @x <=> @y;
-}
-
 =head2 atleast_version
 
 =head2 exact_version
@@ -435,6 +405,51 @@ sub max_version {
     croak "$self has no defined ->version";
 
   return $self->version_cmp($version, $wantver) <= 0;
+}
+
+=head2 version_cmp
+
+  $cmp = Alien::MyLibrary->version_cmp($x, $y)
+
+Comparison method used by L<atleast_version>, L<exact_version> and
+L<max_version>. May be useful to implement custom comparisons, or for
+subclasses to overload to get different version comparison semantics than the
+default rules, for packages that have some other rules than the F<pkg-config>
+behaviour.
+
+Should return a number less than, equal to, or greater than zero; similar in
+behaviour to the C<< <=> >> and C<cmp> operators.
+
+=cut
+
+# Sort::Versions isn't quite the same algorithm because it differs in
+# behaviour with leading zeroes.
+#   See also  https://dev.gentoo.org/~mgorny/pkg-config-spec.html#version-comparison
+sub version_cmp {
+  shift;
+  my @x = (shift =~ m/([0-9]+|[a-z]+)/ig);
+  my @y = (shift =~ m/([0-9]+|[a-z]+)/ig);
+
+  while(@x and @y) {
+    my $x = shift @x; my $x_isnum = $x =~ m/[0-9]/;
+    my $y = shift @y; my $y_isnum = $y =~ m/[0-9]/;
+
+    if($x_isnum and $y_isnum) {
+      # Numerical comparison
+      return $x <=> $y if $x != $y;
+    }
+    elsif(!$x_isnum and !$y_isnum) {
+      # Alphabetic comparison
+      return $x cmp $y if $x ne $y;
+    }
+    else {
+      # Of differing types, the numeric one is newer
+      return $x_isnum - $y_isnum;
+    }
+  }
+
+  # Equal so far; the longer is newer
+  return @x <=> @y;
 }
 
 =head2 install_type
