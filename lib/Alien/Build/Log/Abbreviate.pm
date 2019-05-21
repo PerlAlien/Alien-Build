32c8f57997488614e2cfa15164dacead5c57e958
Graham Ollis <plicease@cpan.org>
2019-05-21 13:56:35 -0400

    wip with color

--- lib/Alien/Build/Log/Abbreviate.pm
+++ lib/Alien/Build/Log/Abbreviate.pm
@@ -3,6 +3,7 @@ package Alien::Build::Log::Abbreviate;
 use strict;
 use warnings;
 use 5.008001;
+use Term::ANSIColor ();
 use base qw( Alien::Build );
 
 # ABSTRACT: Log class for Alien::Build which is less verbose
@@ -22,16 +23,29 @@ Send single log line to stdout.
 
 =cut
 
+sub colored
+{
+  -t STDOUT ? Term::ANSIColor::colored(@_) : '';
+}
+
 sub log
 {
   my(undef, %args) = @_;
   my($message) = $args{message};
   my ($package, $filename, $line) = @{ $args{caller} };
 
-  $package =~ s/^Alien::Build::Auto::[^:]+::Alienfile/alienfile/;
-  $package =~ s/^Alien::Build::Plugin/ABP/;
-  $package =~ s/^Alien::Build/AB/;
-  printf "%s:%04d> %s\n", $package, $line, $message;
+  my $source = $package;
+  $source =~ s/^Alien::Build::Auto::[^:]+::Alienfile/alienfile/;
+  $source =~ s/^Alien::Build::Plugin/ABP/;
+  $source =~ s/^Alien::Build/AB/;
+
+  print colored([ "bold on_black"          ], '[');
+  print colored([ "bright_green on_black"  ], $source);
+  print colored([ "on_black"               ], ' ');
+  print colored([ "bright_yellow on_black" ], $line);
+  print colored([ "bold on_black"          ], ']');
+  print colored([ "white on_black"         ], ' ', $message);
+  print "\n";
 }
 
 1;
