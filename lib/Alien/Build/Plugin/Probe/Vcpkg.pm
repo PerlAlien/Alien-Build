5a2b36283d9c69edb1ed4c4ca82fe79c9f278025
Graham Ollis <plicease@cpan.org>
2020-03-08 05:53:05 -0600

    add Vcpkg plugin

---
+++ lib/Alien/Build/Plugin/Probe/Vcpkg.pm
@@ -0,0 +1,107 @@
+package Alien::Build::Plugin::Probe::Vcpkg;
+
+use strict;
+use warnings;
+use Alien::Build::Plugin;
+
+# ABSTRACT: Probe for system libraries using Vcpkg
+# VERSION
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 PROPERTIES
+
+=head2 name
+
+Specifies the name of the Vcpkg.  This should not be used with the C<lib> property below, choose only one.
+
+=head2 lib
+
+Specifies the list of libraries that make up the Vcpkg.  This should not be used with the C<name> property
+above, choose only one.  Note that using this detection method, the version number of the package will
+not be automatically determined (since multuple packages could potentially make up the list of libraries),
+so you need to determine the version number another way if you need it.
+
+This must be an array reference.
+
+=cut
+
+has 'name';
+has 'lib';
+
+sub init
+{
+  my($self, $meta) = @_;
+
+  $meta->add_requires('configure' => 'Alien::Build::Plugin::Probe::Vcpkg' => 0 );
+
+  if($meta->prop->{platform}->{compiler_type} eq 'microsoft')
+  {
+    $meta->register_hook(
+      probe => sub {
+        my($build) = @_;
+        eval {
+          require Win32::Vcpkg;
+          require Win32::Vcpkg::List;
+          require Win32::Vcpkg::Package;
+          Win32::Vcpkg->VERSION('0.02');
+        };
+        if(my $error = $@)
+        {
+          $build->log("unable to load Win32::Vcpkg: $error");
+          return 'share';
+        }
+
+        my $package;
+        if($self->name)
+        {
+          $package = Win32::Vcpkg::List->new
+                                       ->search($self->name);
+        }
+        elsif($self->lib)
+        {
+          $DB::single = 1;
+          $package = eval { Win32::Vcpkg::Package->new( lib => $self->lib ) };
+          return 'share' if $@;
+        }
+        else
+        {
+          $build->log("you must provode either name or lib property for Probe::Vcpkg");
+          return 'share';
+        }
+
+        my $version = $package->version;
+        $version = 'unknown' unless defined $version;
+
+        $build->install_prop->{plugin_probe_vcpkg} = {
+          version => $version,
+          cflags  => $package->cflags,
+          libs    => $package->libs,
+        };
+        return 'system';
+      },
+    );
+
+    $meta->register_hook(
+      gather_system => sub {
+        my($build) = @_;
+        if(my $c = $build->install_prop->{plugin_probe_vcpkg})
+        {
+          $build->runtime_prop->{version} = $c->{version} unless defined $build->runtime_prop->{version};
+          $build->runtime_prop->{$_} = $c->{$_} for qw( cflags libs );
+        }
+      },
+    );
+  }
+}
+
+1;
+
+=head1 SEE ALSO
+
+L<Alien::Build>, L<alienfile>, L<Alien::Build::MM>, L<Alien>
+
+=cut
+
---
+++ t/alien_build_plugin_probe_vcpkg.t
@@ -0,0 +1,123 @@
+use Test2::V0 -no_srand => 1;
+use Test::Alien::Build;
+use Alien::Build::Plugin::Probe::Vcpkg;
+use Path::Tiny;
+
+skip_all 'Test requires Win32::Vcpkg 0.02'
+  unless eval { require Win32::Vcpkg; Win32::Vcpkg->VERSION('0.02') };
+
+$ENV{PERL_WIN32_VCPKG_ROOT}  = path('corpus','alien_build_plugin_probe_vcpkg', 'root1')->absolute->stringify;
+$ENV{PERL_WIN32_VCPKG_DEBUG} = 0;
+$ENV{VCPKG_DEFAULT_TRIPLET}  = 'x64-windows';
+
+alien_subtest 'non vc' => sub {
+
+  require Alien::Build::Plugin::Core::Setup;
+  my $mock = mock 'Alien::Build::Plugin::Core::Setup' => (
+    after => [
+      _platform => sub {
+        my(undef, $hash) = @_;
+        $hash->{compiler_type} = 'unix';
+      },
+    ],
+  );
+
+  alienfile_ok q{
+    use alienfile;
+
+    plugin 'Probe::Vcpkg' => (
+      lib => ['foo'],
+    );
+  };
+
+  alien_install_type_is 'share';
+
+};
+
+subtest 'vc' => sub {
+
+  my $mock = mock 'Alien::Build::Plugin::Core::Setup' => (
+    after => [
+      _platform => sub {
+        my(undef, $hash) = @_;
+        $hash->{compiler_type} = 'microsoft';
+        return;
+      },
+    ],
+  );
+
+  alien_subtest 'lib = foo' => sub {
+
+    my $build = alienfile_ok q{
+      use alienfile;
+
+      plugin 'Probe::Vcpkg' => (
+        lib => ['foo'],
+      );
+    };
+
+    alien_install_type_is 'system';
+    alien_build_ok;
+
+    is(
+      $build->runtime_prop,
+      hash {
+        field version => 'unknown';
+        field cflags  => T();
+        field libs    => T();
+        etc;
+      },
+    );
+
+    note "version = ", $build->runtime_prop->{version};
+    note "cflags  = ", $build->runtime_prop->{cflags};
+    note "libs    = ", $build->runtime_prop->{libs};
+  };
+
+  alien_subtest 'lib = bar' => sub {
+
+    my $build = alienfile_ok q{
+      use alienfile;
+
+      plugin 'Probe::Vcpkg' => (
+        lib => ['bar'],
+      );
+    };
+
+    alien_install_type_is 'share';
+
+  };
+
+  alien_subtest 'name = libffi' => sub {
+
+    local $ENV{PERL_WIN32_VCPKG_ROOT}  = path('corpus','alien_build_plugin_probe_vcpkg', 'root2')->absolute->stringify;
+
+    my $build = alienfile_ok q{
+      use alienfile;
+
+      plugin 'Probe::Vcpkg' => (
+        name => 'libffi',
+      );
+    };
+
+    alien_install_type_is 'system';
+    alien_build_ok;
+
+    is(
+      $build->runtime_prop,
+      hash {
+        field version => '3.3';
+        field cflags  => T();
+        field libs    => T();
+        etc;
+      },
+    );
+
+    note "version = ", $build->runtime_prop->{version};
+    note "cflags  = ", $build->runtime_prop->{cflags};
+    note "libs    = ", $build->runtime_prop->{libs};
+  };
+
+};
+
+done_testing;
