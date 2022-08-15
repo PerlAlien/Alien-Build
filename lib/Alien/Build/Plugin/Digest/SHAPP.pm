56ccc6c8fe92c057989184b1a49aff75db341cf7
Graham Ollis <plicease@cpan.org>
2022-08-15 05:15:44 -0600

    Add Digest::SHAPP

---
+++ lib/Alien/Build/Plugin/Digest/SHAPP.pm
@@ -0,0 +1,56 @@
+package Alien::Build::Plugin::Digest::SHAPP;
+
+use strict;
+use warnings;
+use Alien::Build::Plugin;
+use Capture::Tiny qw( capture_merged );
+
+# DESCRIPTION: Plugin to check SHA digest with Digest::SHA::PurePerl
+# VERSION
+
+=head1 SYNOPSIS
+
+ use alienfile;
+ plugin 'Digest::SHAPP';
+
+=head1 DESCRIPTION
+
+This plugin is experimental.
+
+=cut
+
+sub init
+{
+  my($self, $meta) = @_;
+
+  $meta->add_requires('configure' => 'Alien::Build'          => "2.57" );
+  $meta->add_requires('share'     => 'Digest::SHA::PurePerl' =>    "0" );
+
+  $meta->register_hook( check_digest => sub {
+    my($build, $file, $algo, $expected_digest) = @_;
+
+    my $sha = Digest::SHA::PurePerl->new($algo);
+    return 0 unless defined $sha;
+
+    if(defined $file->{content})
+    {
+      $sha->add($file->{content});
+    }
+    elsif(defined $file->{path})
+    {
+      $sha->addfile($file->{path}, "b");
+    }
+    else
+    {
+      die "unknown file type";
+    }
+
+    my $actual_digest = $sha->hexdigest;
+
+    return 1 if $expected_digest eq $actual_digest;
+    die "@{[ $file->{filename} ]} SHA@{[ $sha->algorithm ]} digest does not match: got $actual_digest, expected $expected_digest";
+
+  });
+}
+
+1;
