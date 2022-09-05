e102b6dc8d3505d47365cf17df1bc097736f8e6a
Graham Ollis <plicease@cpan.org>
2022-09-05 08:35:43 -0600

    more alien_diag improvements

--- lib/Test/Alien/Diag.pm
+++ lib/Test/Alien/Diag.pm
@@ -27,37 +27,84 @@ like C<clfags> and C<libs>.
 
 =head2 alien_diag
 
- alien_diag $alien;
+ alien_diag @aliens;
 
-prints out diagnostics.
+prints out diagnostics for each given alien.  Each alien must be the class
+name of an alien.
+
+[version 2.68]
+
+ alien_diag @aliens, \%options;
+
+Starting with L<Alien::Build> 2.68, you can provide an option hash to adjust the
+behavior of C<alien_diag>.  Valid options are:
+
+=over 4
+
+=item properties
+
+Additional properties to display in the diagnostic.  Useful when you have an L<Alien>
+with custom properties defined in the subclass.
+
+=item list_properties
+
+Additional properties that are returned as a list to display in the diagnostic.  Useful
+when you have an L<Alien> with customer properties that return a list.
+
+=back
 
 =cut
+
+my @default_scalar_properties = qw(
+  cflags cflags_static libs libs_static version install_type
+);
+
+my @default_list_properties = qw(
+  dynamic_libs bin_dir
+);
 
 sub alien_diag ($@)
 {
   my $ctx = context();
 
+  my %options = defined $_[-1] && ref($_[-1]) eq 'HASH' ?  %{ pop @_ } : ();
+
+  my @extra_properties      = @{ delete $options{properties}      || [] };
+  my @extra_list_properties = @{ delete $options{list_properties} || [] };
+
   my $max = 0;
   foreach my $alien (@_)
   {
-    foreach my $name (qw( cflags cflags_static libs libs_static version install_type dynamic_libs bin_dir ))
+    foreach my $name (@default_scalar_properties, @default_list_properties, @extra_properties, @extra_list_properties)
     {
-      my $str = "$alien->$name";
-      if(length($str) > $max)
+      if(eval { $alien->can($name) })
       {
-        $max = length($str);
+        my $str = "$alien->$name";
+        if(length($str) > $max)
+        {
+          $max = length($str);
+        }
       }
     }
   }
 
 
   $ctx->diag('');
+
+  if(%options)
+  {
+    my @extra = sort keys %options;
+    $ctx->diag("warning: unknown option@{[ @extra > 1 ? 's' : '' ]} for alien_diag: @extra");
+    $ctx->diag("(you should check for typos or maybe upgrade to a newer version of Alien::Build)");
+  }
+
+
   foreach my $alien (@_) {
     $ctx->diag('') for 1..2;
 
     my $found = 0;
 
-    foreach my $name (qw( cflags cflags_static libs libs_static version install_type ))
+    foreach my $name (sort(@default_scalar_properties, @extra_properties))
     {
       if(eval { $alien->can($name) })
       {
@@ -68,7 +115,7 @@ sub alien_diag ($@)
       }
     }
 
-    foreach my $name (qw( dynamic_libs bin_dir ))
+    foreach my $name (sort(@default_list_properties, @extra_list_properties))
     {
       if(eval { $alien->can($name) })
       {
@@ -80,9 +127,9 @@ sub alien_diag ($@)
     }
 
     $ctx->diag("no diagnostics found for $alien") unless $found;
-
-    $ctx->diag('') for 1..2;
   }
+
+  $ctx->diag('') for 1..2;
 
   $ctx->release;
 }
