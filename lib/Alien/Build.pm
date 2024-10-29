2f7e5d58e704c6762f2e2a4ad0066b9bf03e5acb
shawnlaffan <shawnlaffan@gmail.com>
2024-10-29 12:09:23 +1100

    support clean_install on system installs also
    This ensures a share install files from
    a previous install are cleaned up.

--- lib/Alien/Build.pm
+++ lib/Alien/Build.pm
@@ -1973,17 +1973,12 @@ Clean files from the final install location.  The default implementation removes
 files recursively except for the C<_alien> directory.  This is helpful when you have
 an old install with files that may break the new build.
 
-For a non-share install this doesn't do anything.
-
 =cut
 
 sub clean_install
 {
   my($self) = @_;
-  if($self->install_type eq 'share')
-  {
-    $self->_call_hook("clean_install");
-  }
+  $self->_call_hook("clean_install");
 }
 
 =head2 system
